fa286ec55cee66ec7122f56610d23dd839783ad9
abhattFSD <161577240+abhattFSD@users.noreply.github.com>
2024-03-03 21:22:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convert = document.getElementById("convert");
+const inputNumber = document.getElementById("inputNumber");
+let mtr = document.getElementById("mtr-con");
+let ltr = document.getElementById("ltr-con");
+let kilo = document.getElementById("kilo-con");
+
+convert.addEventListener("click", function () {
+    let mtrValue = ltrValue = kiloValue = 0;
+    let number = inputNumber.value;
+    mtrValue = (Number(number)*3.281).toFixed(3);
+    feetValue = (Number(number)/3.281).toFixed(3);
+    ltrValue = (Number(number)*0.264).toFixed(3);
+    gallonValue = (Number(number)/0.264).toFixed(3);
+    poundValue = (Number(number)*2.204).toFixed(3);
+    kiloValue = (Number(number)/2.204).toFixed(3);
+    
+    mtr.textContent = `${number} meters = ${mtrValue} feet | ${number} feet = ${feetValue} meters`;
+    ltr.textContent = `${number} liters = ${ltrValue} gallon | ${number} gallon = ${gallonValue} liters`;
+    kilo.textContent = `${number} kilos = ${kiloValue} pound | ${number} pound = ${poundValue} kilo`;
+});
